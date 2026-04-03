6d6c1cfb32608a05c23cf01f56d789c6af14ff3d
Adam Stefanik <adamm.stefanik11@gmail.com>
2026-04-03 14:04:38 +0200

    Remove nested duplicate folder

--- UTB.Minute/UTB.Minute.Contracts/OrderDto.cs
+++
@@ -1,5 +0,0 @@
-namespace UTB.Minute.Contracts;
-
-public record OrderDto(int Id, string Status, DateTime CreatedAt, int MenuItemId, string MealDescription);
-public record CreateOrderDto(int MenuItemId);
-public record UpdateOrderStatusDto(string Status);
--- UTB.Minute/UTB.Minute.Db/OrderStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace UTB.Minute.Db;
-
-public enum OrderStatus
-{
-    Preparing,
-    Ready,
-    Cancelled,
-    Completed
-}
